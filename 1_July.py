80f92f5a01c251b74df29a1dee88cf7943d7e9a5
Roshini <56362238+roshbhaskar@users.noreply.github.com>
2020-07-01 23:10:17 +0530

    Update 1_July.py

--- 1_July.py
+++ 1_July.py
@@ -1,3 +1,28 @@
+'''
+Example 1:
+
+n = 5
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤
+
+Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+n = 8
+
+The coins can form the following rows:
+¤
+¤ ¤
+¤ ¤ ¤
+¤ ¤
+
+Because the 4th row is incomplete, we return 3.
+
+'''
+
 class Solution:
     def arrangeCoins(self, n: int) -> int:
         a = n*2
